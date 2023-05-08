a174b42300f8a26819534e49efa7f3408eaf7452
Sungmin Hong <117700630+dragong-sm@users.noreply.github.com>
2023-05-08 14:17:26 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,29 @@
+function solution(arr, divisor) {
+    var answer = [];
+    
+    arr.forEach((el) => {
+        if(el % divisor == 0){
+            answer.push(el);
+        }
+    })
+    
+    if(answer.length == 0){
+        answer.push(-1);
+    }else{
+        for(let i = 0; i < answer.length; i++){
+            for(let j = 0; j< answer.length; j++){
+                if(answer[i] < answer[j]){
+                    const temp = answer[j];
+                    answer[j] =  answer[i];
+                    answer[i] = temp;
+                }
+            }
+            
+        }
+    }
+    
+    
+    
+    return answer;
+}
+
