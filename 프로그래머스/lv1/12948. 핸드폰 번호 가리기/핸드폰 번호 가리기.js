5f62230f63c5e217f66815befe81a1d80768cb27
Sungmin Hong <117700630+dragong-sm@users.noreply.github.com>
2023-06-03 23:49:37 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,10 @@
 function solution(phone_number) {
+    const count = phone_number.substr(0,phone_number.length-4).length;
+    let star = ""
     
-    for(let i=0; i<phone_number.length-4; i++){
-            phone_number = phone_number.replace(phone_number[i], "*")
+    for(let i=0; i < count ; i++){
+        star += "*"
     }
-
-    return phone_number;
+    
+    return phone_number.replace(phone_number.substr(0,phone_number.length-4), star);
 }
