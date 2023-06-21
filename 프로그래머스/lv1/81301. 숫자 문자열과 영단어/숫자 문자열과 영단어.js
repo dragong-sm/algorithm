bd43c0987ef957887a1b3ea5be0b444f34aca2b8
Sungmin Hong <117700630+dragong-sm@users.noreply.github.com>
2023-06-22 00:03:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -4,21 +4,21 @@ function solution(s) {
     
     const length = Object.keys(obj).length;
     
-    for(let i = 0; i < length + 1; i++){
-        console.log(Object.keys(obj)[i], i)
-       s =  s.replace(Object.keys(obj)[i], i);
+    for(let i = 0; i <= length; i++){
+        const word = Object.keys(obj)[i];
+       s =  s.split(word).join(i);
     }
     
-        s = s.replace("zero", 0);
-        s = s.replace("one", 1);
-        s = s.replace("two",2);
-        s = s.replace("three", 3);
-        s = s.replace("four", 4);
-        s = s.replace("five", 5);
-        s = s.replace("six", 6);
-        s = s.replace("seven", 7);
-        s = s.replace("eight", 8);
-        s = s.replace("nine", 9);
+        // s = s.replace("zero", 0);
+        // s = s.replace("one", 1);
+        // s = s.replace("two",2);
+        // s = s.replace("three", 3);
+        // s = s.replace("four", 4);
+        // s = s.replace("five", 5);
+        // s = s.replace("six", 6);
+        // s = s.replace("seven", 7);
+        // s = s.replace("eight", 8);
+        // s = s.replace("nine", 9);
    
     return parseInt(s);
 }
